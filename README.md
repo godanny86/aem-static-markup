0c41946706773b663238cab41f1b38b6842cd07c
Daniel Gordon <dgordon@adobe.com>
2018-01-30 22:30:30 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Static AEM Markup
+====================================
+
+A static repository with AEM Core Component markup that can be used by front-end developers to style.
+
+## How To Use
+
+    $ git clone git@github.com:scotch-io/gulp-and-less-starter-kit.git
+    $ cd gulp-and-less-starter-kit
+    $ npm install
+    
+After, run
+
+    $ gulp
+    
+Or 
+
+    $ gulp server
+    
+...if you need a local server. Folder **build** serving at [http://localhost:8888](http://localhost:8888)
+
+Should use [Livereload extension](http://livereload.com/extensions/). Or inject `<script src="//localhost:35729/livereload.js"></script>` into your page.
+
+When you change a LESS(or JS) file, the page will reload.
+
+## Updates
+
+**If this isn't working**, it's probably because you need to update. Just run `npm update --save-dev`
+
+**If no command gulp found?**, you need to install it globally `npm install -g gulp` or run `npm run gulp`
+
+### Still broken or not working?
+
+Try this:
+
+```javascript
+sudo npm cache clean
+npm install --save-dev
+npm update --save-dev
+gulp
+```
+
+
+## Quick Tips
+- Any changes in `assets/less/*` will trigger the Less to compile on save
+- All files in `assets/js/libs/*`  will be compressed to `build/jquery.plugins.min.js`
+- All files in `assets/js/*` (except for `libs`) will be compressed to `build/scripts.min.js`
+
+
+#### Special Thanks
+
+To scotch-io for providing a sample gulp project: https://github.com/scotch-io/gulp-and-less-starter-kit
+
+HTML5 UP @jlkn for providing some inspiration html styles: https://html5up.net/
---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+// Load Gulp
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    plugins = require('gulp-load-plugins')({
+        rename: {
+            'gulp-live-server': 'serve'
+        }
+    });
+
+// Start Watching: Run "gulp"
+gulp.task('default', ['watch']);
+
+// Run "gulp server"
+gulp.task('server', ['serve', 'watch']);
+
+// Minify Custom JS: Run manually with: "gulp build-js"
+gulp.task('build-js', function () {
+    return gulp.src(['clientlibs/js/util.js',
+                     'clientlibs/components/image/polyfills.js',
+                     'clientlibs/components/image/image.js',
+                     'clientlibs/components/sharing/sharing.js',
+                     'clientlibs/components/search/search.js',
+                     'clientlibs/components/navigation/navigation.js'])
+        .pipe(plugins.concat('base.js'))
+        .pipe(gulp.dest('build'));
+});
+
+// Less to CSS: Run manually with: "gulp build-css"
+gulp.task('build-css', function () {
+    return gulp.src('clientlibs/base.less')
+        .pipe(plugins.plumber())
+        .pipe(plugins.less())
+        .on('error', function (err) {
+            gutil.log(err);
+            this.emit('end');
+        })
+        .pipe(plugins.autoprefixer({
+            browsers: [
+                    '> 1%',
+                    'last 2 versions',
+                    'firefox >= 4',
+                    'safari 7',
+                    'safari 8',
+                    'IE 8',
+                    'IE 9',
+                    'IE 10',
+                    'IE 11'
+                ],
+            cascade: false
+        }))
+        .pipe(gulp.dest('build')).on('error', gutil.log);
+});
+
+// Default task
+gulp.task('watch', function () {
+    gulp.watch('clientlibs/**/*.js', ['build-js']);
+    gulp.watch('clientlibs/**/*.less', ['build-css']);
+});
+
+// Folder "/" serving at http://localhost:8888
+// Should use Livereload (http://livereload.com/extensions/)
+gulp.task('serve', function () {
+    var server = plugins.serve.static('/', 8888);
+    server.start();
+    gulp.watch(['build/*'], function (file) {
+        server.notify.apply(server, [file]);
+    });
+});
